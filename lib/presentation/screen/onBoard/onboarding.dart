a296ce497f5d1735827dca296a6e8690b76cd159
paresh <pareshbhalekar812@gmail.com>
2023-11-22 19:52:44 +0530

    first commit

---
+++ lib/presentation/screen/onBoard/onboarding.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class onBoard extends StatelessWidget {
+  const onBoard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
